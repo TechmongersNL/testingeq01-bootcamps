5ca1b431c8f414a5374660a89bf483727da49c22
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2023-11-07 16:57:13 +0100

    Set up initial classes

---
+++ src/org/bootcamp/exercises/Bout.java
@@ -0,0 +1,6 @@
+package org.bootcamp.exercises;
+
+public class Bout {
+    int iterations;
+    Exercise exercise;
+}
---
+++ src/org/bootcamp/exercises/Exercise.java
@@ -0,0 +1,5 @@
+package org.bootcamp.exercises;
+
+public class Exercise {
+    String name;
+}
